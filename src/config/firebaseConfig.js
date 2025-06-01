216da4d5391ffff7924cb9266d677c436b17d4dd
Rodri-117 <rodrifaccini@gmail.com>
2025-06-01 02:37:37 -0300

    correccion credencial

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,15 +1,15 @@
 import { initializeApp } from "firebase/app";
-import {getFirestore} from "firebase/firestore"
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDG02KVh2QmACpkSQTW3051fHjzsCE-mR8",
-    authDomain: "react-js-final-55acf.firebaseapp.com",
-    projectId: "react-js-final-55acf",
-    storageBucket: "react-js-final-55acf.firebasestorage.app",
-    messagingSenderId: "143735469829",
-    appId: "1:143735469829:web:4555f225e4002ecc78696c",
-    measurementId: "G-MWZ046CPLB"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app)
+export const db = getFirestore(app);
